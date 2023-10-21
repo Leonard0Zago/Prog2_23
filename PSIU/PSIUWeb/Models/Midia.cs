181c2d4bf57513b974fbe343a596f5acaee8e942
Leonardo <110700602+Leonard0Zago@users.noreply.github.com>
2023-10-21 18:27:30 -0300

    MidiaAdicionada

---
+++ PSIU/PSIUWeb/Models/Midia.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PSIUWeb.Models
+{
+    public class Midia
+    {
+        [Key]
+        public int MidiaID { get; set; }
+        public string URL { get; set; }
+        public string TipoMidia { get; set; }
+    }
+}
